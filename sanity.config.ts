eab7feb77e87db21819f1595e9e5044293e98d28
heyrajesh123 <sangmarajesh219@gmail.com>
2026-03-06 20:07:57 +0530

    Create sanity.config.ts

---
+++ sanity.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { gameSchema } from "./schemaTypes/game";
+
+export default defineConfig({
+  name: "gamevault-studio",
+  title: "GameVault Studio",
+  projectId: "eq6o0luu",
+  dataset: "production",
+  plugins: [structureTool()],
+  schema: {
+    types: [gameSchema],
+  },
+});
